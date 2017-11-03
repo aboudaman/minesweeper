5ec80d895ce5aba626a5731379815c6fa5c91e9a
aboudaman <adaman2000@gmail.com>
2017-11-03 14:24:02 -0400

    added 2d array to game

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,12 +1,16 @@
 /* jshint esversion: 6 */
-const blankLine = ' | | ';
-const guessLine = '1| | ';
-const bombLine = ' |B| ';
-console.log ("This is what an empty board would look like:");
-console.log(blankLine);
-console.log(blankLine);
-console.log(blankLine);
-console.log ("This is what an empty board would look like:");
-console.log(guessLine);
-console.log(bombLine);
-console.log(blankLine);
+const board = [
+  [' ', ' ', ' '],
+  [' ', ' ', ' '],
+  [' ', ' ', ' ']
+];
+const printBoard = board => {
+  console.log('Current Board');
+  console.log(board[0].join(' | '));
+  console.log(board[1].join(' | '));
+  console.log(board[2].join(' | '));
+}
+printBoard(board);
+board[0][1] = 1;
+board[2][2] = 'B';
+printBoard(board);
